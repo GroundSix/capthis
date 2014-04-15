42afcd229b7003685f577f7d2500fd0cda9f4795
Harry Lawrence <hazbo@gmx.com>
2014-04-15 16:34:40 +0100

    Tabs to spaces

--- capthis_test.go
+++ capthis_test.go
@@ -1,26 +1,26 @@
 package capthis
 
 import (
-	"fmt"
-	"testing"
-	"./capthis"
+    "fmt"
+    "testing"
+    "./capthis"
 )
 
 func TestNew(t *testing.T) {
-	caption := capthis.New()
-	if (fmt.Sprintf("%T", caption) != "*capthis.Caption") {
-		t.Error("Expected type *capthis.Caption")
-	}
+    caption := capthis.New()
+    if (fmt.Sprintf("%T", caption) != "*capthis.Caption") {
+        t.Error("Expected type *capthis.Caption")
+    }
 
-	if (fmt.Sprintf("%T", caption.MagicWand()) != "*imagick.MagickWand") {
-		t.Error("Expected type *imagick.MagickWand")
-	}
+    if (fmt.Sprintf("%T", caption.MagicWand()) != "*imagick.MagickWand") {
+        t.Error("Expected type *imagick.MagickWand")
+    }
 
-	if (fmt.Sprintf("%T", caption.PixelWand()) != "*imagick.PixelWand") {
-		t.Error("Expected type *imagick.PixelWand")
-	}
+    if (fmt.Sprintf("%T", caption.PixelWand()) != "*imagick.PixelWand") {
+        t.Error("Expected type *imagick.PixelWand")
+    }
 
-	if (fmt.Sprintf("%T", caption.DrawingWand()) != "*imagick.DrawingWand") {
-		t.Error("Expected type *imagick.DrawingWand")
-	}
+    if (fmt.Sprintf("%T", caption.DrawingWand()) != "*imagick.DrawingWand") {
+        t.Error("Expected type *imagick.DrawingWand")
+    }
 }
