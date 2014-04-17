51e8fbb379ba40ce26edaeafd62bd7a19cc8ed3d
Harry Lawrence <hazbo@gmx.com>
2014-04-17 11:33:37 +0100

    Tabs to spaces

--- capthis/validator.go
+++ capthis/validator.go
@@ -17,8 +17,8 @@
 package capthis
 
 import (
-	"log"
-	"encoding/json"
+    "log"
+    "encoding/json"
     "net/http"
 )
 
@@ -32,15 +32,15 @@ import (
  * @return string errors or empty string
  */
 func ReportErrors(r *http.Request) string {
-	errors := validatePostFields(r)
-	if (len(errors) > 0) {
-		jsonString, err := json.Marshal(errors)
-		if (err != nil) {
-			log.Fatal(err)
-		}
-		return string(jsonString[:])
-	}
-	return ""
+    errors := validatePostFields(r)
+    if (len(errors) > 0) {
+        jsonString, err := json.Marshal(errors)
+        if (err != nil) {
+            log.Fatal(err)
+        }
+        return string(jsonString[:])
+    }
+    return ""
 }
 
 /**
@@ -52,33 +52,33 @@ func ReportErrors(r *http.Request) string {
  * @return []string slice of errors
  */
 func validatePostFields(r *http.Request) []string {
-	errors := []string{}
-	if (r.PostFormValue("image_name") == "") {
-		errors = append(errors, "image_name must be defined")
-	}
+    errors := []string{}
+    if (r.PostFormValue("image_name") == "") {
+        errors = append(errors, "image_name must be defined")
+    }
 
-	if (r.PostFormValue("text") == "") {
-		errors = append(errors, "text must be defined")
-	}
+    if (r.PostFormValue("text") == "") {
+        errors = append(errors, "text must be defined")
+    }
 
-	if (r.PostFormValue("font") == "") {
-		errors = append(errors, "font must be defined")
-	}
+    if (r.PostFormValue("font") == "") {
+        errors = append(errors, "font must be defined")
+    }
 
-	if (r.PostFormValue("font_size") == "") {
-		errors = append(errors, "font_size must be defined")
-	}
+    if (r.PostFormValue("font_size") == "") {
+        errors = append(errors, "font_size must be defined")
+    }
 
-	if (r.PostFormValue("fill_color") == "") {
-		errors = append(errors, "fill_color must be defined")
-	}
+    if (r.PostFormValue("fill_color") == "") {
+        errors = append(errors, "fill_color must be defined")
+    }
 
-	if (r.PostFormValue("stroke_color") == "") {
-		errors = append(errors, "stroke_color must be defined")
-	}
+    if (r.PostFormValue("stroke_color") == "") {
+        errors = append(errors, "stroke_color must be defined")
+    }
 
-	if (r.PostFormValue("output") == "") {
-		errors = append(errors, "output must be defined")
-	}
-	return errors
+    if (r.PostFormValue("output") == "") {
+        errors = append(errors, "output must be defined")
+    }
+    return errors
 }
